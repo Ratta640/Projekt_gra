8a951c56af690ffe8e61b7806e377c579ffde752
Ratta640 <s31043@pjwstk.edu.pl>
2025-05-12 13:43:50 +0200

    Gra_CiagDalszy_12.05
    funkcjonalna gra Bulls and Cows

--- Assets/Scripts/gamecontroller.cs
+++ Assets/Scripts/gamecontroller.cs
@@ -2,19 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class gamecontroller : MonoBehaviour
 {
-    public string slowo = "zdanie";
-    public TextMeshProUGUI MainZdanie;
-   public TMP_InputField InputField;
+    public string Word = "after";
+    public int LivesCount = 10;
+    public TextMeshProUGUI MainWord;
+    public TextMeshProUGUI SuggestionText;
+    public TMP_InputField InputField;
     public TextMeshProUGUI OurInput;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        MainZdanie.text = slowo;
+        MainWord.text = Word;
+        OurInput.text = $"Zostalo prób: {LivesCount} ";
     }
 
     // Update is called once per frame
@@ -29,5 +33,49 @@ public class gamecontroller : MonoBehaviour
         OurInput.text = input;
 
     }
+    public void OnClick()
+    {
+        Debug.Log($"Przycisk klikniety");
+        LivesCount = LivesCount - 1;
+        OurInput.text = $"Zostalo prób: {LivesCount} ";
+
+        if (Word == InputField.text)
+        {
+            Debug.Log($"Poprawna odpowiedz");
+            //TODO Change text on canvas
+            return;
+        }
+        if (Word.Length != InputField.text.Length)
+        {
+            SuggestionText.text = $"Dlugosc sie nie zgadza";
+            return;
+        }
+
+        int bullsCount = CountBulls();
+        int cowsCount = CountCows();
+
+        SuggestionText.text = $"Bulls: {bullsCount} and Cows: {cowsCount}";
+    }
+    public int CountBulls()
+    {
+        int result = 0;
+
+        for (int i = 0; i < Word.Length; i++)
+        {
+            // || (or) =>conajmniej jedna ze stron jest true => true, else false
+            //&& (and) warunki po lewej i prawej powinny byc true => true, else false
+           if (Word[i] != InputField.text[i] && Word.Contains(InputField.text[i]))
+            {
+                result++;
+            }
+             
+        }
+
+        return result;
+    }
+    
+    public int CountCows()       
+    {
+        return 0;
+    }
 }
-s
